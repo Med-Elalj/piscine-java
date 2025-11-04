e502e5f0cd956f2f49ba6df344fdff7135d319b3
melalj <mohammedelalj@outlook.com>
2025-11-04 18:54:50 +0100

    StringConcat

---
+++ StringConcat/StringConcat.java
@@ -0,0 +1,14 @@
+
+public class StringConcat {
+
+    public static String concat(String s1, String s2) {
+        if (s1 == null && s2 == null) {
+            return null;
+        }else if (s1 == null) {
+            s1 = "";
+        }else if (s2 == null) {
+            s2 = "";
+        }
+        return s1 + s2;
+    }
+}
