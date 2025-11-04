70194e64b9028ce103cd218e51ebb44deb5273eb
melalj <mohammedelalj@outlook.com>
2025-11-04 18:54:14 +0100

    StringLength

---
+++ StringLength/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println(StringLength.getStringLength("Hello Wérld !"));
+        System.out.println(StringLength.getStringLength(null));
+    }
+}
