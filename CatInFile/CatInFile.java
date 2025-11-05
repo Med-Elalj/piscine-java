4eab7e1b913725afe7bee8554f967d105d02ca5f
melalj <mohammedelalj@outlook.com>
2025-11-05 19:12:56 +0100

    CatInFile

---
+++ CatInFile/CatInFile.java
@@ -0,0 +1,18 @@
+
+import java.io.*;
+
+public class CatInFile {
+
+    public static void cat(String[] args) throws IOException {
+        if (args == null || args.length == 0) {
+            return;
+        }
+        byte[] data = System.in.readAllBytes();
+        String file = args[0];
+        try (OutputStream inputStream = new FileOutputStream(file)) {
+            inputStream.write(data);
+            inputStream.close();
+        }
+
+    }
+}
