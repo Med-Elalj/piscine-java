ad1fa54b5a4ee3995ab6a2fd36abacfd141f5256
melalj <mohammedelalj@outlook.com>
2025-11-06 16:18:21 +0100

    StarOverride

---
+++ StarOverride/Star.java
@@ -0,0 +1,40 @@
+public class Star extends CelestialObject {
+    private double magnitude;
+    
+    public double getMagnitude() {
+        return magnitude;
+    }
+
+    public void setMagnitude(double magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public Star() {
+        super();
+        this.magnitude = 0.0;
+    }
+
+    public Star(String name, double x, double y, double z, double magnitude) {
+        super(name, x, y, z);
+        this.magnitude = magnitude;
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.hashCode(magnitude);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Star)) return false;
+        Star other = (Star) obj;
+        return Double.compare(this.magnitude, other.magnitude) == 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s shines at the %.3f magnitude", this.name, this.magnitude);
+    }
+
+}
