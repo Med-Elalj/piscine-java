1da3869a9e25cfab56e183f2934718c58afa851f
melalj <mohammedelalj@outlook.com>
2025-11-06 14:39:14 +0100

    StarConstructors

---
+++ StarConstructors/CelestialObject.java
@@ -0,0 +1,21 @@
+public class CelestialObject {
+    double x;
+    double y;
+    double z;
+    String name;
+
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+}
