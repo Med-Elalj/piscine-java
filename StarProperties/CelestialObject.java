eb2efa43bc505d5ad44d9fc5707a2da49bc2c583
melalj <mohammedelalj@outlook.com>
2025-11-06 14:49:51 +0100

    me < retard

--- StarProperties/CelestialObject.java
+++ StarProperties/CelestialObject.java
@@ -5,26 +5,13 @@ public class CelestialObject {
     public String name;
 
     public CelestialObject() {
-        this.x = 0.0;
-        this.y = 0.0;
-        this.z = 0.0;
         this.name = "Soleil";
     }
 
-    public CelestialObject(double x, double y, double z, String name) {
+    public CelestialObject( String name, double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "CelestialObject{" +
-                "x=" + x +
-                ", y=" + y +
-                ", z=" + z +
-                ", name='" + name + '\'' +
-                '}';
     }
 }
