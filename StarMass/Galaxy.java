a8442ef4000daac3be2ad93a95a397a4d74a551c
melalj <mohammedelalj@outlook.com>
2025-11-06 17:05:10 +0100

    StarMass

---
+++ StarMass/Galaxy.java
@@ -0,0 +1,41 @@
+import java.util.List;
+import java.util.Map;
+
+public class Galaxy {
+    private List<CelestialObject>  celestialObjects;
+
+    public Galaxy() {
+        this.celestialObjects = new java.util.ArrayList<>();
+    }
+
+    public List<CelestialObject> getCelestialObjects() {
+        return celestialObjects;
+    }
+
+    public void addCelestialObject(CelestialObject obj) {
+        this.celestialObjects.add(obj);
+    }
+
+    public Map<String, Integer> computeMassRepartition() {
+        Map<String, Integer> massRepartition = new java.util.HashMap<>();
+        int totalMassStars = 0;
+        int totalMassPlanets = 0;
+        int totalMassOthers = 0;
+
+        for (CelestialObject obj : celestialObjects) {
+            if (obj instanceof Star) {
+                totalMassStars += obj.getMass();
+            } else if (obj instanceof Planet) {
+                totalMassPlanets += obj.getMass();
+            } else {
+                totalMassOthers += obj.getMass();
+            }
+        }
+
+        massRepartition.put("Others", totalMassOthers);
+        massRepartition.put("Planets", totalMassPlanets);
+        massRepartition.put("Stars", totalMassStars);
+
+        return massRepartition;
+    }
+}
