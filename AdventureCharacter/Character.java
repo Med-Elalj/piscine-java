4aa7ecd39fc56d4def1b185c6753c91e83a4d940
melalj <mohammedelalj@outlook.com>
2025-11-13 18:49:13 +0100

    Adventure Character

---
+++ AdventureCharacter/Character.java
@@ -0,0 +1,42 @@
+public class Character {
+    private final int maxHealth;
+    private int currentHealth;
+    private final String name;
+
+    public Character(String name, int maxHealth) {
+        this.name = name;
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void takeDamage(int amount) {
+        if (amount <= 0) return;
+        currentHealth -= amount;
+        if (currentHealth < 0) currentHealth = 0;
+    }
+
+    public void attack(Character target) {
+        if (target == null) return;
+        target.takeDamage(9);
+    }
+
+    @Override
+    public String toString() {
+        if (currentHealth == 0) {
+            return name + " : KO";
+        }
+        return name + " : " + currentHealth + "/" + maxHealth;
+    }
+}
