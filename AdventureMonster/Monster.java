f4b3c81a18853b13e2debd5036ab0b04ca92b171
melalj <mohammedelalj@outlook.com>
2025-11-13 19:04:43 +0100

    AdventureMonster

---
+++ AdventureMonster/Monster.java
@@ -0,0 +1,19 @@
+public class Monster extends Character {
+
+    public Monster(String name, int maxHealth) {
+        super(name, maxHealth);
+    }
+
+    @Override
+    public String toString() {
+        if (this.isAlive()) {
+            return this.getName() + " is a monster with " + this.getCurrentHealth() + " HP.";
+        } else {
+            return this.getName() + " is a monster and is dead.";
+        }
+    }
+
+    public boolean isAlive() {
+        return this.getCurrentHealth() > 0;
+    }
+}
