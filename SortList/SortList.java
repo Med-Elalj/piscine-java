ddbd5ef34e0b52963cbbefc72aad8d2708139ecd
melalj <mohammedelalj@outlook.com>
2025-11-15 12:07:02 +0100

    Sort List

---
+++ SortList/SortList.java
@@ -0,0 +1,20 @@
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortList {
+
+    public static List<Integer> sort(List<Integer> list) {
+        if (list == null) {
+            return null;
+        }
+        return list.stream().sorted().toList();
+    }
+
+    public static List<Integer> sortReverse(List<Integer> list) {
+        if (list == null) {
+            return null;
+        }
+        return list.stream().sorted(Comparator.reverseOrder()).toList();
+    }
+}
