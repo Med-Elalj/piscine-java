490a627393a7227ecdc11b34130d1259d7340506
melalj <mohammedelalj@outlook.com>
2025-11-15 11:29:36 +0100

    null ref

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -1,12 +1,16 @@
 import java.util.List;
+import java.util.Objects;
 
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
+        if (list1 == list2) {
+            return true;
+        }
         if (list1.size() != list2.size()) {
             return false;
         }
         for (int i = 0; i < list1.size(); i++) {
-            if (!list1.get(i).equals(list2.get(i))) {
+            if (!Objects.equals(list1.get(i), list2.get(i))) {
                 return false;
             }
         }
