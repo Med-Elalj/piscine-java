444f7051053d33bc980c9e92d2e01a0b79e6bfc1
melalj <mohammedelalj@outlook.com>
2025-11-17 18:25:20 +0100

    Format Date

---
+++ FormatDate/FormatDate.java
@@ -0,0 +1,31 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class FormatDate {
+    public static String formatToFullText(LocalDateTime dateTime) {
+        if (dateTime == null) {
+            return null;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("'Le' dd MMM 'de l''an' yyyy 'à' HH'h'mm'm et' ss's'", Locale.FRENCH);
+        return dateTime.format(formatter);
+    }
+
+    public static String formatSimple(LocalDate date) {
+        if (date == null) {
+            return null;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd yy", Locale.ITALIAN);
+        return date.format(formatter);
+    }
+
+    public static String formatIso(LocalTime time) {
+        if (time == null) {
+            return null;
+        }
+        return time.format(DateTimeFormatter.ISO_LOCAL_TIME);
+    }
+
+}
