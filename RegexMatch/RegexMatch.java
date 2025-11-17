7b0198c854c5f71aa2964fa1dcd6dd45603d7232
melalj <mohammedelalj@outlook.com>
2025-11-17 18:27:09 +0100

    RegexMatch

---
+++ RegexMatch/RegexMatch.java
@@ -0,0 +1,22 @@
+public class RegexMatch {
+    public static boolean containsOnlyAlpha(String s) {
+        if (s == null) {
+            return false;
+        }
+        return s.matches("^[a-zA-Z]*$");
+    }
+
+    public static boolean startWithLetterAndEndWithNumber(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        return s.matches("^[a-zA-Z].*[0-9]$");
+    }
+
+    public static boolean containsAtLeast3SuccessiveA(String s) {
+        if (s == null) {
+            return false;
+        }
+        return s.contains("AAA");
+    }
+}
